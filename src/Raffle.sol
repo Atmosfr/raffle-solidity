7bdbc3c00caa17910ec69898578247aba67e5223
AlexGer <1999bag@gmail.com>
2025-03-12 00:35:54 +0300

    feat: make ownable

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract Raffle {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Raffle is Ownable {
     /* ERRORS */
     /* TYPES */
     /* IMMUTABLES */
     /* STORAGE */
+    address private lastWinner;
+
     /* EVENTS */
     /* MODIFIERS */
     /* CONSTRUCTOR */
+    constructor(address owner) Ownable(owner) {}
     /* EXTERNAL FUNCTIONS */
     /* PUBLIC FUNCTIONS */
     /* INTERNAL FUNCTIONS */
