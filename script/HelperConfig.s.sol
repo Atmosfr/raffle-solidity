548b3fd4151d61bb9266c0025dc41ed5dba478f5
AlexGer <1999bag@gmail.com>
2025-03-18 18:32:36 +0300

    feat: add Constants & refactor getOrCreateAnvilEthConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,16 +5,21 @@ import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 import {MockLinkToken} from "@chainlink/contracts/src/v0.8/mocks/MockLinkToken.sol";
 
-contract HelperConfig is Script {
+contract Constants {
+    int256 internal constant MOCK_WEI_PER_UINT_LINK = 4e15;
+    uint256 internal constant FUND_AMOUNT = 100 ether;
+    uint32 internal constant CALLBACK_GAS_LIMIT = 500_000;
+    uint256 internal constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 internal constant LOCAL_CHAIN_ID = 31337;
+
+    uint96 internal constant MOCK_BASE_FEE = 0.25 ether;
+    uint96 internal constant MOCK_GAS_PRICE_LINK = 1e9;
+}
+
+contract HelperConfig is Constants, Script {
     error HelperConfig__InvalidChainId();
 
-    uint96 private constant BASE_FEE = 30 gwei;
-    uint96 private constant GAS_PRICE = 10 gwei;
-    int256 private constant WEI_PER_UNIT_LINK = 1 gwei;
-    uint256 private constant FUND_AMOUNT = 100 ether;
-    uint32 private constant CALLBACK_GAS_LIMIT = 500_000;
-    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
-    uint256 public constant LOCAL_CHAIN_ID = 31337;
+    // LINK / ETH price
 
     struct NetworkConfig {
         uint256 subscriptionId;
@@ -23,6 +28,7 @@ contract HelperConfig is Script {
         uint32 callbackGasLimit;
         address linkToken;
         uint256 entranceFee;
+        address account;
     }
 
     NetworkConfig public activeNetworkConfig;
@@ -63,7 +69,8 @@ contract HelperConfig is Script {
                 keyHash: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: CALLBACK_GAS_LIMIT,
                 linkToken: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
-                entranceFee: 0.001 ether
+                entranceFee: 0.001 ether,
+                account: 0x733E5e0E250f9b21F13507F7e3a415eb64e366eb
             });
     }
 
@@ -74,28 +81,28 @@ contract HelperConfig is Script {
 
         vm.startBroadcast();
 
-        // deploy vrfCoordinator mock
-        VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(
-            BASE_FEE,
-            GAS_PRICE,
-            WEI_PER_UNIT_LINK
+        VRFCoordinatorV2_5Mock vrfCoordinatorV2_5 = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UINT_LINK
         );
 
         MockLinkToken linkToken = new MockLinkToken();
 
-        uint256 subId = vrfCoordinator.createSubscription();
-        vrfCoordinator.fundSubscription(subId, FUND_AMOUNT);
+        uint256 subId = vrfCoordinatorV2_5.createSubscription();
 
         vm.stopBroadcast();
 
-        return
-            NetworkConfig({
-                vrfCoordinatorV2_5: address(vrfCoordinator),
-                keyHash: bytes32(0),
-                callbackGasLimit: CALLBACK_GAS_LIMIT,
-                subscriptionId: subId,
-                linkToken: address(linkToken),
-                entranceFee: 0.001 ether
-            });
+        activeNetworkConfig = NetworkConfig({
+            vrfCoordinatorV2_5: address(vrfCoordinatorV2_5),
+            keyHash: bytes32(0),
+            callbackGasLimit: CALLBACK_GAS_LIMIT,
+            subscriptionId: subId,
+            linkToken: address(linkToken),
+            entranceFee: 0.001 ether,
+            account: DEFAULT_SENDER
+        });
+
+        return activeNetworkConfig;
     }
 }
