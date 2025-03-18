9c06f75abcba537f239dd1b4e4e3486e6dca3f11
AlexGer <1999bag@gmail.com>
2025-03-18 18:35:32 +0300

    feat: add test

--- test/unit/Raffle.t.sol
+++ test/unit/Raffle.t.sol
@@ -2,7 +2,39 @@
 pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
+import {DeployRaffle, HelperConfig} from "script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
 
 contract RaffleTest is Test {
-    function setUp() external {}
+    uint8 constant PARTICIPANTS_COUNT = 2;
+
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    address vrfCoordinatorV2_5;
+    bytes32 keyHash;
+    uint32 callbackGasLimit;
+    uint256 subscriptionId;
+    uint256 entranceFee;
+
+    function setUp() external {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        (raffle, helperConfig) = deployRaffle.run(PARTICIPANTS_COUNT);
+        HelperConfig.NetworkConfig memory networkConfig = helperConfig
+            .getConfig();
+
+        vrfCoordinatorV2_5 = networkConfig.vrfCoordinatorV2_5;
+        keyHash = networkConfig.keyHash;
+        callbackGasLimit = networkConfig.callbackGasLimit;
+        subscriptionId = networkConfig.subscriptionId;
+        entranceFee = networkConfig.entranceFee;
+    }
+
+    function test_constructor() public view {
+        assertEq(raffle.getParticipantsCount(), PARTICIPANTS_COUNT);
+        assertEq(raffle.getMinEntryFee(), entranceFee);
+        assertEq(raffle.getKeyHash(), keyHash);
+        assertEq(address(raffle.s_vrfCoordinator()), vrfCoordinatorV2_5);
+        //assertEq(raffle.getSubscriptionId(), subscriptionId);
+    }
 }
